85ea6be52841391b2d72c0d5260f615314d08d9a
alex <103598894+95brsu@users.noreply.github.com>
2024-12-17 22:36:11 +0300

    Update install_hemi.sh

--- install_hemi.sh
+++ install_hemi.sh
@@ -18,5 +18,20 @@ chmod +x ./popmd
 ./keygen -secp256k1 -json -net="testnet" > ~/hemi-keys/popm-address$num_p.json
 cat ~/hemi-keys/popm-address$num_p.json
 
-echo "Запросите тестовые токены tBTC3 на адрес pubkey_hash"
-echo "Продолжите установку следую инструкциям из README"
+show "Автоматическое присваивание переменных из JSON..."
+eval $(jq -r '. | "ETHEREUM_ADDRESS'$num_p'=\(.ethereum_address)\nNETWORK=\(.network)\nPRIVATE_KEY'$num_p'=\(.private_key)\nPUBLIC_KEY'$num_p'=\(.public_key)\nPUBKEY_HASH'$num_p'=\(.pubkey_hash)"' ~/popm-address'$num_p'.json)
+
+show "Вывод переменных..."
+echo "Ethereum Address: $ETHEREUM_ADDRESS$num_p"
+echo "Network: $NETWORK"
+echo "Private Key: $PRIVATE_KEY$num_p"
+echo "Public Key: $PUBLIC_KEY$num_p"
+echo "Public Key Hash: $PUBKEY_HASH$num_p"
+
+show "Экспорт переменных окружения..."
+export POPM_BTC_PRIVKEY=PRIVATE_KEY$num_p
+export POPM_STATIC_FEE=50
+export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public
+
+show "Запросите тестовые токены tBTC3 на адрес pubkey_hash"
+show "Продолжите установку следую инструкциям из README"
